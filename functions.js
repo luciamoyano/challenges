89768bf8ae346210bdc5c71e3663da25c8fef60a
Lucia Moyano <luciamoyano2@gmail.com>
2020-07-29 23:42:03 -0300

    Update of functions.js
    - Var to const
    - New variables: nodoPrecio and nodoEmailInput
    - Optimization of activeHeart function

--- functions.js
+++ functions.js
@@ -1,15 +1,16 @@
 // Precio
 function setPrice() {
-    var metros = 380;
-    var precio = document.getElementById("priceInput").value;
-    var m2 = precio / metros;
+    const nodoPrecio = document.getElementById("priceInput");
+    const metros = 380;
+    const precio = nodoPrecio.value;
+    const m2 = precio / metros;
 
     var avisoM2 = document.getElementsByClassName("precio-metro-numero");
     avisoM2[0].innerHTML = m2.toFixed();
 
     localStorage.setItem("storedPrecio", precio);
     };
-document.getElementById("priceInput").value = localStorage.getItem("storedPrecio");
+    document.getElementById("priceInput").value = localStorage.getItem("storedPrecio");
 
 // Slides
 var slideIndex = 1;
@@ -39,14 +40,14 @@ var heart = document.getElementsByClassName("like");
 var heartActive = false;
 
 function activeHeart() {
-    if (heartActive === false) {
-        heartActive = true;
-        heart[0].style.color = "#ff6257";
-        localStorage.setItem("heartColor", "#ff6257");
-    } else {
+    if (heartActive) {
         heartActive = false;
         heart[0].style.color = "#000";
         localStorage.setItem("heartColor", "#000");
+    } else {
+        heartActive = true;
+        heart[0].style.color = "#ff6257";
+        localStorage.setItem("heartColor", "#ff6257");
     }
 };
 
@@ -54,20 +55,20 @@ heart[0].style.color = localStorage.getItem("heartColor");
     
 // Email
 function validateEmail() {
-    var email = document.getElementById("emailInput").value;
+    const nodoEmailInput = document.getElementById("emailInput");
+    var email = nodoEmailInput.value;
     var resultado = document.getElementById("resultado-email");
-    var emailInput = document.getElementById("emailInput");
     var form = document.getElementById("form-contacto");
 
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         resultado.innerHTML = "¡Gracias! Te responderemos en breve";
         resultado.style.color = "green";
-        emailInput.style.borderColor = "#ccc";
+        nodoEmailInput.style.borderColor = "#ccc";
         form.reset();
     } else {
         resultado.innerHTML = "Por favor, ingrese un email válido";
         resultado.style.color = "red";
-        emailInput.style.borderColor = "red";
+        nodoEmailInput.style.borderColor = "red";
     }
 };
 
